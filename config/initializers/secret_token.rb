a21eccf71e03a1bb26931bbdaf49553f902b5b8d
Shawn Page <bspage@webassign.net>
2013-02-19 16:52:25 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NoDb::Application.config.secret_token = 'dfb34d39b8ffd9cf24e76980756ed9d547be40e36e0c283cbc972a8f8cfcbd7d06886a2f068e9d7b7a2633b4edac782f520906a7ad57797588ae9799670fee43'
